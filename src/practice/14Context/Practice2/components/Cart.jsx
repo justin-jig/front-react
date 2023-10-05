0ea451993dbd170de9574e7d882c7fdf80378c11
justin-jig <justin-j@naver.com>
2023-10-05 15:27:16 +0900

    20231005 context api study

--- src/practice/14Context/Practice2/components/Cart.jsx
+++ src/practice/14Context/Practice2/components/Cart.jsx
@@ -3,14 +3,28 @@ import './PracticeComponent.scss';
 import refrigerator from './image/냉장고.png';
 import cleaner from './image/청소기.png';
 import washing  from './image/세탁기.png';
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { practiceContext } from '../practice.context'
 
 export default function Cart () {
 
     const [cartOnOFF, setCartOnOFF] = useState(false);
     const {cartList, deleteCartList} = useContext(practiceContext);
- 
+    const [cartPrice, setCartPrice] = useState(0);
+
+    useEffect(() => {
+
+        function priceAll() {
+            let price = 0;
+            cartList.forEach((value) => {
+                price +=  value.price;
+            })
+            setCartPrice(price);
+        }
+        priceAll();
+
+    },[cartList])
+
     return (
         <div className='cart-wrap'>
 
@@ -22,6 +36,7 @@ export default function Cart () {
         
             <div className={ cartOnOFF ? 'cart-list displayblock' :'cart-list displaynone'}>
 
+                <p className='pice'>총액 : {cartPrice.toLocaleString()} </p>
                 {cartList.length === 0 ? 
                     <div className='datanone'>장바구니 내역 없음</div>
                     :
